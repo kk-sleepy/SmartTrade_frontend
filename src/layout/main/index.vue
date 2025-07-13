d25376e8d31848c26c592efafd18b3db32424fe8
kk-sleepy <1610896646@qq.com>
2025-07-13 15:57:56 +0800

    校园转赚前端上传

---
+++ src/layout/main/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <!-- 路由组件出口的位置 -->
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <!-- 渲染layout一级路由组件的子路由 -->
+      <component :is="Component" v-if="flag" />
+    </transition>
+  </router-view>
+  <div class="footer">
+    <p>
+      <i class="iconfont icon-banquan" style="font-size: 18px"></i>
+      2025 kksleepy  ——
+      <span>
+        校园转赚
+      </span>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { watch, ref, nextTick } from "vue";
+import useLayOutSettingStore from "@/store/modules/setting";
+
+let layOutSettingStore = useLayOutSettingStore();
+
+//控制当前组件是否销毁重建
+let flag = ref(true);
+
+//监听仓库内部数据是否发生变化,如果发生变化，说明用户点击过刷新按钮
+watch(
+  () => layOutSettingStore.refsh,
+  () => {
+    //点击刷新按钮:路由组件销毁
+    flag.value = false;
+    nextTick(() => {
+      flag.value = true;
+    });
+  }
+);
+</script>
+<script lang="ts">
+export default {
+  name: "Main"
+};
+</script>
+
+<style scoped lang="scss">
+.fade-enter-from {
+  opacity: 0;
+  transform: scale(0);
+}
+
+.fade-enter-active {
+  transition: all 0.3s;
+}
+
+.fade-enter-to {
+  opacity: 1;
+  transform: scale(1);
+}
+
+.footer {
+  width: calc(100% - $base-menu-width);
+  margin: 10px 0px;
+  text-align: center;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
